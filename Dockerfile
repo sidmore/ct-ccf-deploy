4b95551b8d3fc2d2d86c4664e770b2f5621d3eec
Cale Teeter <cale.teeter@microsoft.com>
2022-11-01 09:59:44 -0400

    Update to move cert/key handling to entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM mcr.microsoft.com/ccf/app/dev:2.0.8-sgx
 
-COPY cert /opt/ccf/bin/
-COPY key /opt/ccf/bin/
 COPY entrypoint.sh actions/deploy/entrypoint.sh
 
 ENTRYPOINT ["/actions/deploy/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,9 @@
 
 cp $1 /opt/ccf/bin
 
+echo "$CERTD" > /opt/ccf/bin/cert
+echo "$KEYD" > /opt/ccf/bin/key
+
 cd /opt/ccf/bin
 content=$(./scurl.sh ${CCF_URL}gov/proposals -k --signing-cert cert --signing-key key -X POST -H "Content-Type: application/json" --data-binary @set_js_app.json)
 proposal=$(echo "${content}" | jq '.proposal_id')
