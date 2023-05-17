37b19f5f8a1b9a79a14e73199e71b8c9a131c7e0
Siddharth More <simore@microsoft.com>
2023-05-17 13:45:53 -0700

    Fix PR Comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ echo "$CERTD" > /opt/ccf/bin/cert
 echo "$KEYD" > /opt/ccf/bin/key
 
 cd /opt/ccf/bin
-content=$(ccf_cose_sign1 --ccf-gov-msg-type proposal --ccf-gov-msg-created_at `date -Is` --signing-key key --signing-cert cert --content set_js_app.json | curl ${CCF_URL}/gov/proposals --cacert service_cert.pem --data-binary @set_js_app.json -H "content-type: application/cose")
+content=$(ccf_cose_sign1 --ccf-gov-msg-type proposal --ccf-gov-msg-created_at `date -Is` --signing-key key --signing-cert cert --content set_js_app.json | curl ${CCF_URL}/gov/proposals -k -H "content-type: application/cose" --data-binary @-)
 proposal=$(echo "${content}" | jq '.proposal_id')
 echo "proposal=$proposal" >> $GITHUB_OUTPUT
